43b0c14565a6c5adf25c3043daff040cd4febdae
Aleksandr <menshikov.av@mail.ru>
2024-10-14 18:30:56 +0400

    [2] mocks; app flex fix for desktop; dashboard with mock data; run script downgraded to node 16;

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,32 +1,23 @@
 import React from 'react';
+import {POSTS_MOCK} from "../../mocks/posts.mock";
+import {PHOTOS_MOCK} from "../../mocks/photos.mock";
+import {ALBUMS_MOCK} from "../../mocks/albums.mock";
 
 export function Dashboard() {
-    let postsCount = 5;
-    let albumsCount = 6;
-    let photosCount = 7;
+    let postsCount = POSTS_MOCK.length;
+    let albumsCount = ALBUMS_MOCK.length;
+    let photosCount = PHOTOS_MOCK.length;
+    let latestPosts = POSTS_MOCK.slice(POSTS_MOCK.length - 5);
+    let latestPhotos = PHOTOS_MOCK.slice(PHOTOS_MOCK.length - 5);
     return (<>
-            {/*<div>Dashboard</div>*/}
-
             <div className="px-5 py-3 bg-gray-600 text-white" data-ref="header">
                 Welcome to
                 <h1>My App</h1>
             </div>
 
             <div className="p-1">
-
-                {/*<div *ngIf="(isLoading$|async) === true"*/}
-                {/*data-ref="loading-indicator"*/}
-                {/*className="spinner-border m-2"*/}
-                {/*role="status">*/}
-                {/*<span className="visually-hidden">Loading...</span>*/}
-                {/*</div>*/}
-
-                {/*<app-error-msg *ngIf="state?.error && !state.isLoading" [error]="state.error"></app-error-msg>*/}
-
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 ">
-
-                    {/*<div className="columns-md col-md-6 p-2">*/}
-                    <div data-ref="statistics-card" className="border border-solid border-gray-500 p-2 rounded-lg">
+                    <div className="border border-solid border-gray-500 p-2 rounded-lg">
                         <div className="font-bold pb-1">Statistics</div>
                         <div className="card-body">
                             <div className="row text-nowrap">
@@ -42,44 +33,33 @@ export function Dashboard() {
                             </div>
                         </div>
                     </div>
-                    {/*</div>*/}
 
-                    {/*<div className="col-lg-4 col-md-6 p-2">*/}
-                    <div data-ref="statistics-card" className="border border-solid border-gray-500 p-2 rounded-lg">
+                    <div className="border border-solid border-gray-500 p-2 rounded-lg">
                         <div className="font-bold pb-1">Latest Posts</div>
                         <div className="card-body">
-                            latest posts items here
-                            {/*    <div *ngFor="let post of state.latestPosts"*/}
-                            {/*    className="text-truncate overflow-hidden py-1"*/}
-                            {/*    >*/}
-                            {/*    <a [routerLink]="'/posts/'+post.id">{{post.title}}</a>*/}
-                            {/*<div className="text-truncate overflow-hidden">{{post.body}}</div>*/}
+                            {latestPosts.map(post =>
+                                <div key={post.id} className={"mt-3"}>
+                                    <a href={"/posts/" + post.id}
+                                       className={"line-clamp-2 text-blue-600 visited:text-purple-600"}>{post.title}</a>
+                                    <div className="line-clamp-2 overflow-hidden">{post.body}</div>
+                                </div>
+                            )}
                         </div>
                     </div>
-                    {/*</div>*/}
-                    {/*</div>*/}
 
-                    {/*<div className="col-lg-4 col-md-6 p-2">*/}
-                    <div data-ref="statistics-card" className="border border-solid border-gray-500 p-2 rounded-lg">
+                    <div className="border border-solid border-gray-500 p-2 rounded-lg">
                         <div className="font-bold pb-1">Recent Photos</div>
-                        <div className="card-body">
-                            recent photos list
-                            {/*<div className="row">*/}
-                            {/*    <div *ngFor="let photo of state.latestPhotos"*/}
-                            {/*    className="col-4 p-3">*/}
-                            {/*    <img [src]="photo.thumbnailUrl" className="img-thumbnail">*/}
-                            {/*    <div className="text-truncate overflow-hidden bg-light">*/}
-                            {/*        <a [routerLink]="'/photos/'+photo.id">{{photo.title}}</a>*/}
-                            {/*    </div>*/}
-                            {/*</div>*/}
+                        <div className="card-body grid grid-cols-3 gap-2">
+                            {latestPhotos.map(photo =>
+                                <a href={"/photos/" + photo.id} key={photo.id}>
+                                    <img src={photo.thumbnailUrl} alt="photo" className="w-[50px] h-[50px]"/>
+                                    <div className={"line-clamp-2"}>{photo.title}</div>
+                                </a>)}
                         </div>
                     </div>
-                    {/*</div>*/}
                 </div>
-                {/*</div>*/}
 
             </div>
         </>
-
     );
 }
