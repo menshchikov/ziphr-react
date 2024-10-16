37274d5507dd9aa2fd05750de9eb812bfed0a895
Aleksandr <menshikov.av@mail.ru>
2024-10-16 19:57:38 +0400

    #19 photo list page query;

--- src/services/photo-api.ts
+++ src/services/photo-api.ts
@@ -1,22 +1,15 @@
 import {Photo} from "../model/photo";
 
-export function getPhotoById(id: string):Promise<Photo> {
+export function getPhotoById(id: string): Promise<Photo> {
     const url = 'https://jsonplaceholder.typicode.com/photos/';
     return fetch(url + id).then(res => res.json())
 }
 
-export function getPhotos(options?:{albumId?: string, title?: string}){
+export function getPhotos(filterType: string, filterValue: string) {
     let url = 'https://jsonplaceholder.typicode.com/photos';
-    if(options?.albumId){
-        url += '?albumId=' + options?.albumId;
+    if (filterType === 'albumId' && filterValue) {
+        url += '?albumId=' + filterValue;
     }
     return fetch(url)
         .then(res => res.json())
-        .then(photos => {
-            let titleFilter = options?.title || '';
-            if(titleFilter?.length > 0){
-                return photos.filter((photo:Photo) => photo.title.indexOf(titleFilter) > -1);
-            }
-            return photos;
-        })
 }
