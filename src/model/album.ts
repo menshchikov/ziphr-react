8a1e83d9c2e139bb40f0682fae3cba9ecc307368
amenshchikov <menshikov.av@hotmail.com>
2024-10-29 16:51:00 +0400

    AlbumCardPhotos query was replaced with usePhotos hook; redundant model code removing;

--- src/model/album.ts
+++ src/model/album.ts
@@ -1,12 +1,6 @@
-import { Photo } from "./photo";
-
 export interface Album {
   userId: number,
   id: number,
   title: string,
-}
-
-export interface AlbumWithPhotos extends Album{
-  photos: Photo[]
 }
 
--- src/model/post.ts
+++ src/model/post.ts
@@ -4,8 +4,3 @@ export interface Post {
   title: string;
   body: string;
 }
-
-export enum PostSearchFieldsEnum {
-  userId = 'userId',
-  title = 'title',
-}
