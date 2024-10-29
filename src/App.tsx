f91ce111936186522dc7f2b29060e97bd703797f
amenshchikov <menshikov.av@hotmail.com>
2024-10-29 05:20:30 +0400

    Albums useCallback wierd solution; app.css removing;

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { Outlet } from 'react-router-dom';
 import { Menu } from './components/Menu/Menu';
 
--- src/components/Albums/Albums.tsx
+++ src/components/Albums/Albums.tsx
@@ -1,4 +1,4 @@
-import {ChangeEvent, useRef} from 'react';
+import {ChangeEvent, useCallback, useMemo} from 'react';
 import {Paginator} from "../Paginator";
 import {useSearchParams} from "react-router-dom";
 import {AlbumCardPhotos} from "./AlbumCardPhotos.tsx";
@@ -23,12 +23,24 @@ export function Albums() {
         setSearchParams(searchParams);
     }
 
- const setSearchParamsDebounced = useCallback(
-    debounce((searchParams) => {
-        setSearchParams(searchParams);
-    }, 500),
-    [setSearchParams]
-);
+    // const setSearchParamsDebounced = useCallback(
+    //     debounce((searchParams) => {
+    //         setSearchParams(searchParams);
+    //     }, 500),
+    //     [setSearchParams]
+    // );
+    // React Hook useCallback received a function whose dependencies are unknown. Pass an inline function instead
+    // solution with useMemo works fine, but looks wierd
+
+    const setSearchParamsDebouncedMemorized = useMemo(() =>
+        debounce((params: URLSearchParams) => {
+            setSearchParams(params);
+        }, 500), [setSearchParams]
+    );
+
+    const setSearchParamsDebounced = useCallback((p: URLSearchParams) =>
+        setSearchParamsDebouncedMemorized(p), [setSearchParamsDebouncedMemorized]
+    );
 
     function onFilterChange(e: ChangeEvent<HTMLInputElement>) {
         const value = e.target.value;
