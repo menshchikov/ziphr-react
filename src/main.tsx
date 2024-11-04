d54c1317b47b722980b75265a282192767d891c2
Aleksandr <menshikov.av@mail.ru>
2024-11-04 13:41:06 +0400

    removing redundant router element braces

--- src/main.tsx
+++ src/main.tsx
@@ -17,21 +17,21 @@ import {NotFound} from './NotFound.tsx';
 
 const router = createBrowserRouter([
     {
-        path: '/', element: (<App/>), children: [
-            {index: true, element: (<Navigate to={'/dashboard'} replace/>)},
-            {path: 'dashboard', element: (<Dashboard/>), index: true},
-            {path: 'posts', element: (<Posts/>)},
-            {path: 'posts/:id', element: (<Post/>)},
-            {path: 'albums', element: (<Albums/>)},
-            {path: 'albums/:id', element: (<Album/>)},
-            {path: 'photos', element: (<Photos/>)},
-            {path: 'photos/:id', element: (<Photo/>)},
-            {path: 'users/:id', element: (<User/>)},
+        path: '/', element: <App/>, children: [
+            {index: true, element: <Navigate to={'/dashboard'} replace/>},
+            {path: 'dashboard', element: <Dashboard/>, index: true},
+            {path: 'posts', element: <Posts/>},
+            {path: 'posts/:id', element: <Post/>},
+            {path: 'albums', element: <Albums/>},
+            {path: 'albums/:id', element: <Album/>},
+            {path: 'photos', element: <Photos/>},
+            {path: 'photos/:id', element: <Photo/>},
+            {path: 'users/:id', element: <User/>},
         ],
-        errorElement: (<NotFound/>),
+        errorElement: <NotFound/>,
     },
     {
-        path: '/', element: (<Navigate to={'/Dashboard'}/>),
+        path: '/', element: <Navigate to={'/Dashboard'}/>,
     },
 ]);
 const queryClient = new QueryClient();
